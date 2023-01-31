09a2c0709af23203f287f35ec94faf45fac9c596
Dhvani Vora <dvora@splunk.com>
2023-01-31 09:42:11 +0530

    Code review changes

--- sandflysecurity_connector.py
+++ sandflysecurity_connector.py
@@ -8,6 +8,7 @@
 from __future__ import print_function, unicode_literals
 
 import json
+import sys
 from datetime import datetime
 
 # Phantom App imports
@@ -56,6 +57,9 @@ class SandflySecurityConnector(BaseConnector):
 
         try:
             soup = BeautifulSoup(response.text, "html.parser")
+            for element in soup(["script", "style", "footer", "nav"]):
+                element.extract()
+
             error_text = soup.text
             split_lines = error_text.split('\n')
             split_lines = [x.strip() for x in split_lines if x.strip()]
@@ -496,7 +500,7 @@ def main():
             session_id = r2.cookies['sessionid']
         except Exception as e:
             print("Unable to get session id from the platform. Error: " + str(e))
-            exit(1)
+            sys.exit(1)
 
     with open(args.input_test_json) as f:
         in_json = f.read()
@@ -513,7 +517,7 @@ def main():
         ret_val = connector._handle_action(json.dumps(in_json), None)
         print(json.dumps(json.loads(ret_val), indent=4))
 
-    exit(0)
+    sys.exit(0)
 
 
 if __name__ == '__main__':
